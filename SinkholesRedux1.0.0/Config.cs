935f77ec23e1114d0b073cca52c7ee8601514754
LilNesquuik <72927329+LilNesquuik@users.noreply.github.com>
2022-07-14 08:31:11 +0200

    Add files via upload

---
+++ SinkholesRedux1.0.0/Config.cs
@@ -0,0 +1,34 @@
+﻿
+
+namespace SinkholesRedux
+{
+    using System.ComponentModel;
+    using Exiled.API.Features;
+    using Exiled.API.Interfaces;
+
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+
+        [Description("The distance from the center of a sinkhole where a player gets teleported. This is limited to inside the sinkhole's range. 3.3 recommanded")]
+        public float TeleportDistance { get; set; } = 3.3f;
+
+        [Description("Creates interference in the site's electrical system when passing through a sinkhole.")]
+        public bool BlackoutOnCorroding { get; set; } = true;
+
+        [Description("The number of seconds of the blackout when passing through a sinkhole.")]
+        public float TurnOffDuration { get; set; } = 0.9f;
+
+        [Description("Whether or not once the player is in the dimension he necessarily dies?.")]
+        public bool KillCorroding { get; set; } = false;
+
+        [Description("The distance from the center of a sinkhole where a player gets teleported. This is limited to inside the sinkhole's range. 2.3 recommanded")]
+        public Broadcast TeleportMessage { get; set; } = new Broadcast
+        {
+            Content = "You've fallen into the pocket dimension!",
+            Duration = 5,
+            Show = false,
+        };
+
+    }
+}
