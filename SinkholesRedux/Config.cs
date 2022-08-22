3a65f54fb9164b389dc3bda3a9f6d585080c76e5
LilNesquuik <72927329+LilNesquuik@users.noreply.github.com>
2022-08-22 15:54:41 +0200

    Add files via upload

---
+++ SinkholesRedux/Config.cs
@@ -0,0 +1,42 @@
+﻿
+
+namespace SinkholesRedux
+{
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using Exiled.API.Enums;
+    using Exiled.API.Features;
+    using Exiled.API.Interfaces;
+    using MEC;
+
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+
+        [Description("The distance from the center of a sinkhole where a player gets teleported. This is limited to inside the sinkhole's range. 3.3 recommanded")]
+        public float TeleportDistance { get; set; } = 4f;
+
+        [Description("Creates interference in the site's electrical system when passing through a sinkhole.")]
+        public bool BlackoutOnCorroding { get; set; } = true;
+
+        [Description("The number of seconds of the blackout when passing through a sinkhole.")]
+        public float TurnOffDuration { get; set; } = 0.9f;
+
+        [Description("The Falling speed (Recommanded 0.8f).")]
+        public float fallingSpeed { get; set; } = 0.8f;
+
+
+        [Description("The broadcast when player fall inside the pocket")]
+        public Broadcast TeleportMessage { get; set; } = new Broadcast
+        {
+            Content = "You've fallen into the pocket dimension!",
+            Duration = 5,
+            Show = false,
+        };
+
+
+
+    }
+
+}
+
---
+++ SinkholesRedux/Plugin.cs
@@ -0,0 +1,31 @@
+﻿namespace SinkholesRedux
+{
+    using System;
+    using Exiled.API.Features;
+
+    public class Plugin : Plugin<Config>
+    {
+        private EventHandlers eventHandlers;
+
+
+        public override string Author => "LilNesquuik";
+        public override Version RequiredExiledVersion { get; } = new Version(5, 3, 0);
+
+        public override Version Version { get; } = new Version(0, 1, 0);
+
+        public override void OnEnabled()
+        {
+            eventHandlers = new EventHandlers(this);
+            Exiled.Events.Handlers.Player.StayingOnEnvironmentalHazard += eventHandlers.OnStayEnvironmentalHazard;
+            base.OnEnabled();
+        }
+
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Player.StayingOnEnvironmentalHazard -= eventHandlers.OnStayEnvironmentalHazard;
+            eventHandlers = null;
+            base.OnDisabled();
+        }
+    }
+}
